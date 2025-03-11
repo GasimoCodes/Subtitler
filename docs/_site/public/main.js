e5dc1694b6a7bcb1e3d92d0970245e8b6524fca6
Gasimo <prokop.svacina@gmail.com>
2025-03-11 14:11:21 +0100

    Update template

--- docs/_site/public/main.js
+++ docs/_site/public/main.js
@@ -1,1 +1,14 @@
-export default {}
+/**
+ * Licensed to the .NET Foundation under one or more agreements.
+ * The .NET Foundation licenses this file to you under the MIT license.
+ */
+
+export default {
+  iconLinks: [
+    {
+      icon: 'house-door',
+      href: 'https://gasimo.dev/',
+      title: 'Home'
+    }
+  ]
+}
